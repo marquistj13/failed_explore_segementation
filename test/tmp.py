4ec8decba777a0ea94ef2786e657f08a3c643a35
Unknown <houpeixin@yeah.net>
2017-11-12 15:44:30 +0800

    enable test

---
+++ test/tmp.py
@@ -0,0 +1,26 @@
+## -*- coding: utf-8 -*-
+import sys
+
+import voc_utils
+
+sys.path.append("..")
+from skimage.io import imread
+from skimage.io import imshow
+import matplotlib.pyplot as plt
+
+
+imgPath = r"E:\VOCtrainval_11-May-2012\VOCdevkit\VOC2012"
+imgSegIndex = imgPath + r"\ImageSets\Segmentation"
+imgSetPath = imgPath + r"\JPEGImages"
+#
+# f = open(imgSegIndex + r"\train.txt", 'r')
+# tmp = f.read()
+# imgSegIndexList = tmp.split('\n')
+# # print imgSegIndexList
+#
+# img=imread(imgSetPath+'\\'+imgSegIndexList[0]+'.jpg')
+# imshow(img)
+# plt.show()
+#
+
+print  voc_utils.list_image_sets()
